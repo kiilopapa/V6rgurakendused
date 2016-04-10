0e32ec7c2771b09dbaf5387f32bff2afa2ab5e5b
Kristjan Peterson <kpeterso@enos.itcollege.ee>
2016-04-10 23:44:04 +0300

    tektivärv

--- 8.nadal/index.php
+++ 8.nadal/index.php
@@ -5,30 +5,39 @@
 	<head>
 		<title>	</title>
 		<meta charset="utf-8" />
-		<style type="text/css">
-
-		</style>
-	
-	</head>
-	<body>
-
-	<?php  
+		<?php  
 		$myurl=$_SERVER['PHP_SELF'];
 		$nimi = "Siia kirjuta tekst";
+		$bg_col="blue"; //taustavärv
+		$color = "skyblue";
 
 		if (!empty($_GET["kuvatav"])){
 			$nimi =  $_GET["kuvatav"];
 		}
 
-		$bg_col="#f0f"; // vaikimisi väärtus
+		 // vaikimisi väärtus
 		if (isset($_GET['bg_color']) && $_GET['bg_color']!="") {
     		$bg_col=htmlspecialchars($_GET['bg_color']);
 		} 
 
+		if (isset($_GET['color']) && $_GET['color']!="") {
+    		$color=htmlspecialchars($_GET['color']);
+		} 
+		?>
+		<style type="text/css">
+			#kuva {
+				background-color : <?php echo $bg_col?>;
+				color: <?php echo $color?>;
 
-	?>
+			}
+		</style>
+	
+	</head>
+	<body>
 
-		<div id="kuva" style="background-color : <?php echo $bg_col;?>">
+	
+
+		<div id="kuva">
 			<?php 
 				
 				if (!empty($_GET["kuvatav"])){
@@ -44,8 +53,10 @@
 
 <form action="<?php echo $myurl?>" method="GET">
 	<input type="text" name="kuvatav" value="<?php echo $nimi;?>" />
-
+	<input type="color" name="color" value="<?php echo $color;?>">
+	Tekstivärvus
 	<input type="color" name="bg_color" value="<?php echo $bg_col;?>">
+	Taustavärv
 	<input type="submit" value="kuva"/>
 </form>
 
