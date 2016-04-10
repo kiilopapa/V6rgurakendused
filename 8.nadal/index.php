ac77029a16ddaf3f64bcc84677b849b43a78c590
Kristjan Peterson <kpeterso@enos.itcollege.ee>
2016-04-10 23:06:39 +0300

    default värv

--- 8.nadal/index.php
+++ 8.nadal/index.php
@@ -8,9 +8,19 @@
 	
 	</head>
 	<body>
-	<?php  $myurl=$_SERVER['PHP_SELF'];?>
 
-		<div id="kuva">
+	<?php  
+		$myurl=$_SERVER['PHP_SELF'];
+
+		$bg_col="#f0f"; // vaikimisi väärtus
+		if (isset($_GET['bg_color']) && $_GET['bg_color']!="") {
+    		$bg_col=htmlspecialchars($_GET['bg_color']);
+		} 
+
+
+	?>
+
+		<div id="kuva" style="color: <?php echo $bg_col;?>">
 			<?php 
 				
 				if (!empty($_GET["kuvatav"])){
@@ -20,16 +30,13 @@
 		</div>
 
 
-<?php $bg_col="#f0f"; // vaikimisi valge
-if (isset($_GET['bg_color']) && $_GET['bg_color']!="") {
-    $bg_col=htmlspecialchars($_GET['bg_color']);
-} ?>
+
 
 
 
 <form action="<?php echo $myurl?>" method="GET">
 	<input type="text" name="kuvatav" />
-	<input type="color" name="bg_color" value="#ff0000">
+	<input type="color" name="bg_color" value="<?php echo $bg_col;?>">
 	<input type="submit" value="kuva"/>
 </form>
 
