4a6b83cc01f7007dc37d98b6595e1ef87b49c9e3
Kristjan Peterson <kpeterso@enos.itcollege.ee>
2016-04-24 22:45:13 +0300

    sessiooni lõpetamine toimib

--- 10.nadal/kontroller.php
+++ 10.nadal/kontroller.php
@@ -1,6 +1,8 @@
 <?php
 require_once("vaated/head.html");
 
+session_start();
+
 $pildid = array(
 		1=>array('src'=>"pildid/nameless1.jpg", 'alt'=>"nimetu 1"),
 		2=>array('src'=>"pildid/nameless2.jpg", 'alt'=>"nimetu 2"),
@@ -20,14 +22,35 @@ switch($page){
 		include("vaated/galerii.html");
 	break;
 	case "vote":
-		include("vaated/vote.html");
+		if (isset($_SESSION['voted_for'])) {
+			$id=htmlspecialchars($_SESSION['voted_for']);
+			include("vaated/jubavalitud.html");
+		} else { 
+			include("vaated/vote.html");
+		}
 	break;
 	case "tulemus":
 		$id=false;
-		if (isset($_POST['pilt']) && isset($pildid[$_POST['pilt']]))
+		if (isset($_POST['pilt']) && isset($pildid[$_POST['pilt']])){
 			$id=htmlspecialchars($_POST['pilt']);
-		include("vaated/tulemus.html");
+			if (!isset($_SESSION['voted_for'])) {
+				$_SESSION['voted_for']=$_POST['pilt'];
+				include("vaated/tulemus.html");
+			} else {
+				$id=htmlspecialchars($_SESSION['voted_for']);
+				include("vaated/jubavalitud.html");
+			}
+		}
 	break;
+	case 'endsession':
+		$_SESSION = array();
+		if (isset($_COOKIE[session_name()])) {
+				setcookie(session_name(), '', time()-42000, '/');
+		}
+		session_destroy();
+		header('Location: ?page=vote');
+		exit;			
+		break;
 	default:
 	 include('vaated/pealeht.html');
 }
